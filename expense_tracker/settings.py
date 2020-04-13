47fe4696d92efd7ff806569cd69a033452568dba
Kaoru Steinboeck <kaoru.steinboeck@uni.li>
2020-04-13 15:11:45 +0200

    added: base template from bootstrap and crispy forms.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
     'budget.apps.BudgetConfig',
     'category.apps.CategoryConfig',
     #'payment.apps.PaymentConfig',
@@ -123,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from payment.views import Payment, AmountInsert
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('payment/', Payment.as_view()),
     path('allPayment/', AmountInsert)
