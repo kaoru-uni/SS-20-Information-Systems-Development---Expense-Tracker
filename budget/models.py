335c4b9fbd35a910146f0f307530d2de5486f1b0
Gerhard Naegele <gerhard.naegele@gmail.com>
2020-04-13 15:39:49 +0200

    Fixed Failure in budget/admin.py and category/admin.py registration of models was missing.

--- budget/models.py
+++ budget/models.py
@@ -5,7 +5,7 @@ class Budget(models.Model):
     name = models.CharField(max_length=50)
     amount = models.FloatField()
     description = models.CharField(max_length=200)
-    #category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
 
     def add(self):
         self.save()
