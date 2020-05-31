9eb85badff6f2ee26593aa55006213168e5bef46
Kaoru Steinboeck <kaoru.steinboeck@uni.li>
2020-05-31 10:15:16 +0200

    added: index site. fixed: register on website.

--- register/views.py
+++ register/views.py
@@ -8,10 +8,8 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
+            return redirect("/")
 
-        return redirect("/home")
     else:
         form = RegisterForm()
     return render(response, "register/register.html", {"form": form})
-
-
