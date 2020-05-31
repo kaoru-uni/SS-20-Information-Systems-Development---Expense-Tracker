f7993272df6b3e035bb166e65e5cb9094368839b
Gerhard Naegele <gerhard.naegele@gmail.com>
2020-05-31 21:32:29 +0200

    Category CRUD working.

--- category/urls.py
+++ category/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, CategoryCreateView
+from .views import CategoryListView, CategoryCreateView, CategoryEditView, CategoryDeleteView
 
+#https://stackoverflow.com/questions/58825832/django-delete-button-is-not-redirecting-to-the-correct-path
 urlpatterns = [
     path("", CategoryListView.as_view(), name="categories"),
     path("add/", CategoryCreateView.as_view(), name="add_category"),
+    path("<int:pk>/edit", CategoryEditView.as_view(), name="edit_category"),
+    path("<int:pk>/deletecategory", CategoryDeleteView.as_view()),
 ]
