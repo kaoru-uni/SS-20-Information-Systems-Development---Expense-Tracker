6e9b2fcb7b432313f426b841ec87f8e4bc7155b3
Gerhard Naegele <gerhard.naegele@gmail.com>
2020-05-31 16:20:03 +0200

    Category Add is working. Added User and Date to the Model. TODO: add User and Date to the Database Diagramm.

--- category/views.py
+++ category/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic.edit import CreateView
+from django.views.generic import View, ListView
 from category.models import Category
 
+#https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/
+class CategoryCreateView(CreateView):
+        model = Category
+        fields = ['name']
+        success_url = "/category/add"
+        template_name = "add.html"
+        def form_valid(self, form):
+            form_to_save = form.save(commit=False)
+            form_to_save.user = self.request.user
+            return super(CategoryCreateView, self).form_valid(form)
 
-def category_index(request):
-    categories = Category.objects.all()
-    context = {
-        'categories' : categories
-    }
-    return render(request, 'catergories.html', context)
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "category_form.html"
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
 
 
 
