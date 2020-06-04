f7e44f87afe5d9317a81e6ebfef03019dd2c3b29
Gerhard Naegele <gerhard.naegele@gmail.com>
2020-06-04 10:06:41 +0200

    Added Documentation file: Documentation_EXPENSIO_Expense_Tracker.pdf Refactor Code on different files.

--- budget/views.py
+++ budget/views.py
@@ -1,13 +1,14 @@
-from django.db.models import Sum, Count
-from django.http import Http404
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.views.generic import View, ListView
-from budget.models import Budget
-from transaction_expense.models import Transaction_Expense
-from django.shortcuts import render
-from category.models import Category
 import datetime
 import decimal
+
+from django.db.models import Sum
+from django.http import Http404
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from budget.models import Budget
+from category.models import Category
+from transaction_expense.models import Transaction_Expense
 
 """
 https://simpleisbetterthancomplex.com/tutorial/2020/01/19/how-to-use-chart-js-with-django.html
@@ -15,6 +16,11 @@ https://stackoverflow.com/questions/47501469/django-filtering-by-user/47502441
 https://stackoverflow.com/questions/19138609/django-aggreagtion-sum-return-value-only/37751814
 https://docs.djangoproject.com/en/3.0/ref/models/querysets/#month
 https://stackoverflow.com/questions/1416830/django-actual-month-in-queryset
+budget_pie_chart_data is used to populate the budget.html page.  
+@:param data {}
+@:param today is the current date
+@:param month is the current month from today as a string
+@:param year is the current year from today as a string
 total_budget sums up all budget of a user. 
 budget_left is the difference between the total budget and all transactions of the current month. 
 """
@@ -30,10 +36,10 @@ def budget_pie_chart_data(request):
         "amount__sum"
     ]
     total_transactions = (
-        Transaction_Expense.objects.filter(user=request.user)
-        .filter(date__year=today.year, date__month=today.month)
-        .aggregate(Sum("amount"))["amount__sum"]
-        or 0.00
+            Transaction_Expense.objects.filter(user=request.user)
+            .filter(date__year=today.year, date__month=today.month)
+            .aggregate(Sum("amount"))["amount__sum"]
+            or 0.00
     )
     if total_budget is not None:
         budget_left = decimal.Decimal(total_budget) - total_transactions
@@ -59,13 +65,13 @@ def budget_pie_chart_data(request):
         request,
         "budget/budget.html",
         {
-        "labels": return_labels,
-        "data": return_data,
-        "total_budget": total_budget,
-        "transactions": budget_left,
-        "month": month,
-        "year": year,
-        "budget_data_all" : budget_data_all,
+            "labels": return_labels,
+            "data": return_data,
+            "total_budget": total_budget,
+            "transactions": budget_left,
+            "month": month,
+            "year": year,
+            "budget_data_all": budget_data_all,
         },
     )
 
@@ -103,64 +109,24 @@ def budget_pie_chart_data_dash(request):
         return_labels.append(key)
         return_data.append(value)
     return {
-            "labels": return_labels,
-            "data": return_data,
-            "total_budget": total_budget,
-            "transactions": budget_left,
-            "month": month,
-            "year": year,
+        "labels": return_labels,
+        "data": return_data,
+        "total_budget": total_budget,
+        "transactions": budget_left,
+        "month": month,
+        "year": year,
     }
 
-"""
-Let the user select a Budget Category and shows the budget for that category
-"""
-
-def budget_category(request):
-    data = {}
-    today = datetime.date.today()
-    month = today.strftime("%B")
-    year = today.strftime("%Y")
-    total_budget = Budget.objects.filter(user=request.user).aggregate(Sum("amount"))[
-        "amount__sum"
-    ]
-    total_transactions = (
-            Transaction_Expense.objects.filter(user=request.user)
-            .filter(date__year=today.year, date__month=today.month)
-            .aggregate(Sum("amount"))["amount__sum"]
-            or 0.00
-    )
-    if total_budget is not None:
-        budget_left = decimal.Decimal(total_budget) - total_transactions
-    else:
-        budget_left = 0
-    queryset = Budget.objects.filter(user=request.user).values(
-        "amount", "category_id", "name"
-    )
-    for budget in queryset:
-        name = budget["name"]
-        if name not in data:
-            data[name] = budget["amount"]
-        else:
-            data[name] += budget["amount"]
-    return_labels = []
-    return_data = []
-    for key, value in data.items():
-        return_labels.append(key)
-        return_data.append(value)
-    return {
-            "labels": return_labels,
-            "data": return_data,
-            #"data_category": data_category,
-            "total_budget": total_budget,
-            "transactions": budget_left,
-            "month": month,
-            "year": year,
-    }
+    """
+    | BudgetCreateView is used for creating new transactions by using the generic view for creation.
+    | form_class: is the form which will be used for the user input.
+    | model: is the Model which will be used.
+    | success_url: if the input was successful the user will be redirected to the budget overview page. 
+    | template_name: is the template which will be used for the user input. The budget_add.html is used. 
+    | https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/
+    """
 
 
-
-
-# https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/
 class BudgetCreateView(CreateView):
     model = Budget
     fields = ("name", "amount", "description", "category")
@@ -178,6 +144,14 @@ class BudgetCreateView(CreateView):
         form_to_save = form.save(commit=False)
         form_to_save.user = self.request.user
         return super(BudgetCreateView, self).form_valid(form)
+
+    """
+    | BudgetCreateView is used to show all budgets. 
+    | ListView: is a generic view which offers easier lists with less code to write
+    | model: is the Model which will be used.
+    | template_name: is the template where the data will be mapped to.
+    | https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/
+    """
 
 
 class BudgetDeleteView(DeleteView):
@@ -211,4 +185,3 @@ class BudgetEditView(UpdateView):
         if not budget_found.user == self.request.user:
             raise Http404
         return budget_found
-
